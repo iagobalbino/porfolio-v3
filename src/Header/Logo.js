c210eb794f63f139f65169fbe85d6da03c700e01
Iago Balbino <iago_nobre@hotmail.com>
2021-09-10 11:47:33 -0300

    Primeiro commit

---
+++ src/Header/Logo.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <h1>Iago Balbino</h1>
+  );
+};
+
+export default Logo;
---
+++ src/Header/Nav.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          {/* <a href="#">Sobre</a> */}
+          <span>sobre</span>
+        </li>
+        <li>
+          {/* <a href="#">Portfólio</a> */}
+          <span>Portfólio</span>
+        </li>
+        <li>
+          {/* <a href="#">Contato</a> */}
+          <span>Contato</span>
+        </li>
+        <button>Currículo</button>
+      </ul>
+    </nav>
+  )
+}
+
+export default Nav
